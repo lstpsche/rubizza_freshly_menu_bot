04830b08b8a829eb21cdfa9f0938455944abaf4f
Nikita Silivonchik <lstpsche@gmail.com>
2019-08-18 15:34:01 +0300

    Remove forgotten binding.pry

--- models/order.rb
+++ models/order.rb
@@ -5,7 +5,6 @@ class Order < ActiveRecord::Base
   def like(user_num:)
     update(rating: 1)
     user = User.find_by(number: user_num)
-    binding.pry
     dish.dish_ingredients.each do |dish_ingr|
       u_i = UserIngredient.find_by(user: user, ingredient: dish_ingr.ingredient)
       if u_i
@@ -14,7 +13,6 @@ class Order < ActiveRecord::Base
         UserIngredient.create(user: user, ingredient: dish_ingr.ingredient, score: 1)
       end
     end
-    binding.pry
   end
 
   def dislike(user_num:)
